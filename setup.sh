7619ab760b2df771edcd06536d836cf2edc55ab1
Ho Cheung <hocheung@chromium.org>
2026-06-10 11:36:24 +0800

    M144 STAGE 63 - Make Android ARM64 builds use orderfile

--- setup.sh
+++ setup.sh
@@ -404,7 +404,7 @@ copyAndroid () {
 	printf "\n" &&
 	printf "${YEL}Downloading PGO profiles...${c0}\n" &&
 	cd ${CR_SRC_DIR} &&
-	python3 tools/update_pgo_profiles.py --target=android-desktop-arm64 update --gs-url-base=chromium-optimization-profiles/pgo_profiles &&
+	# python3 tools/update_pgo_profiles.py --target=android-desktop-arm64 update --gs-url-base=chromium-optimization-profiles/pgo_profiles &&
 	python3 tools/update_pgo_profiles.py --target=android-arm32 update --gs-url-base=chromium-optimization-profiles/pgo_profiles &&
 	cd ~/thorium &&
 	printf "\n"
